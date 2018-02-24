04a9b1affeaf7190bb1e25a18ccce00ea7d37792
Michel Blancard <github@michelblancard.fr>
2018-02-24 23:22:41 +0100

    Print number of hits

--- src/routes/search.js
+++ src/routes/search.js
@@ -10,13 +10,28 @@ const MAX_NB_HITS = 50;
 router.get('/', async function(req, res, next) {
   try {
     let hitsData;
+    let nbHits;
 
     const queryString = req.query.q;
     const from = req.query.from || 0;
     const validQueryString = ((typeof queryString) === 'string') && (queryString.length !== 0);
     if (!validQueryString) {
       hitsData = [];
+      nbHits = 0;
     } else {
+
+      const esCountResponse = await esClient.count({
+        index: config.elasticsearch.index_name,
+        type: config.elasticsearch.document_type,
+        body: {
+          query: {
+            match: {
+              content: queryString,
+            },
+          },
+        },
+      });
+      nbHits = esCountResponse.count;
 
       const esResponse = await esClient.search({
         index: config.elasticsearch.index_name,
@@ -39,7 +54,7 @@ router.get('/', async function(req, res, next) {
           },
         },
       });
-      
+
       const hits = esResponse.hits.hits;
       hitsData = hits.map(hit => ({
         href: `/dce/${hit._source.annonce_id}-${hit._source.org_acronym}`,
@@ -51,11 +66,10 @@ router.get('/', async function(req, res, next) {
         }));
     }
 
-    const getPagination = function(queryString, from, pageSize, nbResults) {
-      const isLastPage = nbResults < MAX_NB_HITS;
-
+    const getPagination = function(queryString, from, pageSize, nbHits) {
       const currentPageIndex = Math.round(from / MAX_NB_HITS);
       const previousPageIndex = (currentPageIndex > 0) && (currentPageIndex - 1);
+      const isLastPage = nbHits <= ((currentPageIndex + 1) * pageSize);
       const nextPageIndex = !isLastPage && (currentPageIndex + 1);
 
       const firstPageHref = `/search?q=${queryString}`;
@@ -69,9 +83,9 @@ router.get('/', async function(req, res, next) {
       }
     }
 
-    const pagination = validQueryString && getPagination(queryString, from, MAX_NB_HITS,  hitsData.length);
+    const pagination = validQueryString && getPagination(queryString, from, MAX_NB_HITS,  nbHits);
 
-    res.render('search', { pagination, hitsData });
+    res.render('search', { validQueryString, queryString, nbHits, pagination, hitsData });
 
   } catch (error) {
     return next(error);
