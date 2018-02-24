2fb81ef43eb8c7cd4f649c4a7c4f9b398ab8dc6f
Michel Blancard <github@michelblancard.fr>
2018-02-24 21:37:49 +0100

    Add metadata and search highlights

--- src/routes/search.js
+++ src/routes/search.js
@@ -4,34 +4,50 @@ const config = require('../config');
 
 const router = express.Router();
 
+const MAX_NB_HITS = 50;
+
 
 router.get('/', async function(req, res, next) {
   try {
-    let hitsIds;
+    let hitsData;
 
     const queryString = req.query.q;
     if ((typeof queryString) !== 'string' || queryString.length === 0) {
-      hitsIds = [];
+      hitsData = [];
     } else {
 
       const esResponse = await esClient.search({
         index: config.elasticsearch.index_name,
         type: config.elasticsearch.document_type,
+        size: MAX_NB_HITS,
         body: {
-          _source: false,
+          _source: {
+            excludes: [ 'content' ],
+          },
           query: {
             match: {
               content: queryString,
+            },
+          },
+          highlight : {
+            fields : {
+              content : {},
             },
           },
         },
       });
       
       const hits = esResponse.hits.hits;
-      hitsIds = hits.map(hit => hit._id);
+      hitsData = hits.map(hit => ({
+        href: `/dce/${hit._source.annonce_id}-${hit._source.org_acronym}`,
+        annonce_id: hit._source.annonce_id,
+        org_acronym: hit._source.org_acronym,
+        intitule: hit._source.intitule,
+        highlight: hit.highlight.content.join(' … '),
+        }));
     }
 
-    res.render('search', { hitsIds });
+    res.render('search', { hitsData });
 
   } catch (error) {
     next(error);
