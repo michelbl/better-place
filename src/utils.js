3a47e48f8a2ff66d75df4f526de9195020ff544c
amzac <chatdeau@protonmail.com>
2024-03-19 18:59:06 +0100

    Add more search options + some ui changes

--- src/utils.js
+++ src/utils.js
@@ -6,6 +6,34 @@ function extractFrom(req) {
   return parseInt(req.query.from) || 0;
 }
 
+function isEmpty(stringVar){
+  return stringVar === '' || stringVar === undefined || stringVar === null;
+}
+
+function isAuthorizedQueryParam(queryParam){
+  return [ 'q', 'reference', 'organisme', 'intitule'].includes(queryParam);
+}
+/**
+ * 
+ * @param {*} req 
+ * @param {*} query 
+ */
+function generateMatchQueriesFromRequest(expressQuery){
+  const matchQueries = [];
+  for (let key in expressQuery) {
+    if (key !== 'from' && isAuthorizedQueryParam(key) && typeof expressQuery[key] == 'string' && !isEmpty(expressQuery[key])) {
+      const propMatch = key === 'q' ? 'content' : key;
+      matchQueries.push({
+        match: {
+          [propMatch]: expressQuery[key]
+        }
+      });
+    }
+  }
+  return matchQueries;
+
+}
+
 function getDay(datetimeISOString) {
   return datetimeISOString.split('T')[0];
 }
@@ -21,4 +49,5 @@ module.exports = {
   extractFrom,
   getDay,
   getDocCount,
+  generateMatchQueriesFromRequest
 }
